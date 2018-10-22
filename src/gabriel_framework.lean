1a3412f0b5bb8851bf6c0c256871b5274acf08c2
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-10-22 20:19:07 +0100

    modifying mp binders and order of inputs

--- src/gabriel_framework.lean
+++ src/gabriel_framework.lean
@@ -9,25 +9,23 @@ infixr ` →' `:50 := imp -- right associative
 
 prefix `¬' `:40 := fml.not
 
-#print notation ¬
-#check _root_.not
-
 inductive prf : fml → Type
 | axk (p q) : prf (p →' q →' p)
 | axs (p q r) : prf $ (p →' q →' r) →' (p →' q) →' (p →' r)
 | axX (p q) : prf $ ((¬' q) →' (¬' p)) →' p →' q
-| mp (p q) : prf p → prf (p →' q) → prf q
+| mp {p q} : prf (p →' q) → prf p → prf q -- bracket change
 
 open prf
 
-/-
 -- example usage:
-
+/-
 lemma p_of_p_of_p_of_q (p q : fml) : prf $ (p →' q) →' (p →' p) :=
 begin
-  apply mp (p →' q →' p) ((p →' q) →' p →' p) (axk p q),
-  exact axs p q p
+  apply mp (axs p q p),
+  exact (axk p q)
 end
 -/
+
+lemma Q6a (p : fml) : prf $ p →' p := sorry
 
 theorem Q6b (p : fml) : prf $ p →' ¬' ¬' p := sorry
