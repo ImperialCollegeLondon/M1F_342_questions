0f31b089b2d62afffef3c5ac0b0e7dadb5de623b
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-10-22 20:37:24 +0100

    Q6a term solution

---
+++ src/Q6a_term_solution.lean
@@ -0,0 +1,33 @@
+inductive fml
+| atom (i : ℕ)
+| imp (a b : fml)
+| not (a : fml)
+
+open fml
+
+infixr ` →' `:50 := imp -- right associative
+
+prefix `¬' `:40 := fml.not
+
+inductive prf : fml → Type
+| axk (p q) : prf (p →' q →' p)
+| axs (p q r) : prf $ (p →' q →' r) →' (p →' q) →' (p →' r)
+| axX (p q) : prf $ ((¬' q) →' (¬' p)) →' p →' q
+| mp {p q} : prf (p →' q) → prf p → prf q -- bracket change
+
+open prf
+
+
+-- example usage:
+
+lemma p_of_p_of_p_of_q (p q : fml) : prf $ (p →' q) →' (p →' p) :=
+begin
+  apply mp (axs p q p),
+  exact (axk p q)
+end
+
+lemma Q6a (p : fml) : prf $ p →' p :=
+
+mp (p_of_p_of_p_of_q _ (p →' p)) $ axk p p
+
+theorem Q6b (p : fml) : prf $ p →' ¬' ¬' p := sorry
